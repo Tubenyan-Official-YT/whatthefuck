56697bc19cb4db420b93ec6767399a5ca4ae4aff
튜브냥 Tubenyan <seoejun8@gmail.com>
2026-09-13 00:58:44 +0900

    Add isLocked property to Adventure class

--- source/backend/Adventure.hx
+++ source/backend/Adventure.hx
@@ -5,6 +5,7 @@ class Adventure
 	public var name:String;          // 모험 지역명
 	public var time:Float;           // 걸리는시간(초)
 	public var completed:Bool = false; // 끝났는가
+	public var isLocked:Bool = true;//잠겼는가
 
 	public function new(name:String, time:Float)
 	{
